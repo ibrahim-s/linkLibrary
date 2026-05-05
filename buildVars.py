86ee6b334e3d7d073450a608f7531dd068e29f72
Ibrahim Hamadeh <ibra.hamadeh@hotmail.com>
2026-05-05 17:43:20 +0300

    Update last tested version to 2026.1, and prepare for version 2.2.6.

--- buildVars.py
+++ buildVars.py
@@ -28,11 +28,11 @@ This addon does not come with a default or assigned gesture or shortcut to it
 You can as always add a gesture or change the existed one going to :
 NVDA menu>preferences>inputGestures>Link Library."""),
 	# version
-	addon_version="2.2.5",
+	addon_version="2.2.6",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""Fix a bug in Add a link on the fly feature,
-now you can add a link to an opened library or sublibrary, which wasn't possible before.
+	addon_changelog=_("""- Update last tested version, to comply with recent NVDA release 2026.1.
+- In the addon settings dialog, in the combo box that contains several paths to store addon data, now the whole directory path is displayed and not path label only.
 """),
 	# Author(s)
 	addon_author="ibrahim hamadeh<ibra.hamadeh@hotmail.com>",
@@ -45,7 +45,7 @@ now you can add a link to an opened library or sublibrary, which wasn't possible
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
 	addon_minimumNVDAVersion="2021.1.0",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	addon_lastTestedNVDAVersion="2025.1.0",
+	addon_lastTestedNVDAVersion="2026.1.0",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
--- changelog.md
+++ changelog.md
@@ -1,3 +1,4 @@
-### Changes for 2.2.5 ###
+### Changes for 2.2.6 ###
 
-*	Update the Ukrainian translation, contributed by [@George-br](https://github.com/George-br).
+*	Update last tested version, to comply with recent NVDA release 2026.1.
+*	In the addon settings dialog, in the combo box that contains several paths to store addon data, now the whole directory path is displayed and not path label only.
